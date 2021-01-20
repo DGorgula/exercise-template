c2cbf47686e55ec01dab6049bc7c0914a1bb2dea
DGorgula <gil@dankner.org>
2021-01-20 22:53:00 +0200

    q20 added

--- q20.js
+++ q20.js
@@ -1,11 +1,36 @@
+function checkInclusion(arr, num){
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === num){
+            return true;
+        }
+    }
+    return false;
+}
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
-    output.innerText = out;
+    const data = JSON.parse(input);
+    const duplateValues = [];
+    
+    // [1, 2, 3, 4, 5, 6, 7, 4, 1]
+for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data.length; j++) {
+        if (i === j) {
+            continue;
+        }
+        else if (data[i] === data[j]) {
+            if(checkInclusion(duplateValues, data[i])){
+                break;
+            }
+            else{
+                duplateValues.push(data[i])
+            }
+        }
+    }
 }
+
+    output.innerText = duplateValues;
+}
+
+
