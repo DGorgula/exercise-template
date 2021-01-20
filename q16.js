30187a8aaceb3c88feea6f4a39c6d8b9eab346b3
DGorgula <gil@dankner.org>
2021-01-20 22:10:47 +0200

    q16 solved

--- q16.js
+++ q16.js
@@ -1,11 +1,31 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input1 = document.getElementById("input1").value;
     const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    const out = [];
+    for (let i = input; i <= input1; i++) {
+        // i%4 === 0 
+        if (i%4 === 0 && !(i%100  === 0)) {
+            out.push(i);
+        }
+        else if(i%400 === 0){
+            out.push(i);
+        }
+    }
     output.innerText = out;
+}
+
+
+
+
+
+
+
+
+
+
+function bla() {
+    document.getElementById("input").setAttribute("placeholder", "Start Year");
+    document.getElementById("input1").hidden = false;
+    document.getElementById("input1").setAttribute("placeholder", "End Year");
 }
