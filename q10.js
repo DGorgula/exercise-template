17020ce9f3d8729b8973da8b9799a10ca7b1b5d3
DGorgula <gil@dankner.org>
2021-01-20 21:08:54 +0200

    q10 solved

--- q10.js
+++ q10.js
@@ -1,11 +1,9 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    const array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 
-/**/
-    output.innerText = out;
-}
+    for (let i = 0; i  < array.length; i++) {
+        console.log("Row " + i);
+        for (let j = 0; j < array[i].length; j++) {
+            console.log(array[i][j]);
+        }
+    }
+
