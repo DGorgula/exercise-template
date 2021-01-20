59f711b9c9190c6fb04d67bf94b16090b15cfa72
DGorgula <gil@dankner.org>
2021-01-20 18:25:28 +0200

    q3 solved

--- q3.js
+++ q3.js
@@ -1,11 +1,41 @@
 function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
+    const input = document.getElementById("input").value;
+    const input1 = document.getElementById("input1").value;
+    const output = document.getElementById("output");
+    const data = JSON.parse(input);
+    const out = [];
+    let stopper;
+    
+
+    //  checks which is smaller
+    
+    if (input1 <= data.length) {
+        stopper = input1;
+    }
+    else{
+        stopper = data.length;
+    }
+
+
+    //  putting them in the output variable.
+
+    for (let i = 0; i < stopper; i++) {
+        out.push(data[i]);
+    }
+
+
+
     output.innerText = out;
+}
+
+
+
+
+
+//  not part of the exercize, just making it nicer.
+
+function bla() {
+    document.getElementById("input").setAttribute("placeholder", "Array");
+    document.getElementById("input1").setAttribute("placeholder", "number");
 }
